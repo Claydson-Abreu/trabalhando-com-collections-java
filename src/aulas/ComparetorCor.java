763b53ec11d4ad170d9041f28bce8da0bf54761f
Claydson-Abreu <dev.claydson@gmail.com>
2022-02-04 11:01:15 -0300

    Lists

---
+++ src/aulas/ComparetorCor.java
@@ -0,0 +1,10 @@
+package aulas;
+
+import java.util.Comparator;
+
+public class ComparetorCor implements Comparator<Gato> {
+    @Override
+    public int compare(Gato gato1, Gato gato2) {
+        return gato1.getCor().compareToIgnoreCase(gato2.getCor());
+    }
+}
---
+++ src/aulas/ComparetorIdade.java
@@ -0,0 +1,12 @@
+package aulas;
+
+import java.util.Comparator;
+
+public class ComparetorIdade implements Comparator<Gato> {
+    @Override
+    public int compare(Gato gato1, Gato gato2) {
+        return Integer.compare(gato1.getIdade(), gato2.getIdade());
+    }
+
+
+}
